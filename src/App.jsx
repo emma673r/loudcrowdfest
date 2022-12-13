5fedd24c3938f5b1b7940d778e41c9724298dec0
Anna Maria Lester <anna47c4@stud.kea.dk>
2022-12-13 11:39:08 +0100

    burger menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import "./burger-menu-style.css";
 function App() {
   return (
     <div className="App">
+      <Menu right width={"100%"} noOverlay>
+        <a id="home" className="menu-item" href="/">
+          HOME
+        </a>
+        <a id="schedule" className="menu-item" href="/schedule">
+          SCHEDULE
+        </a>
+        <a id="lineup" className="menu-item" href="/lineup">
+          LINE-UP
+        </a>
+        <a id="booking" className="menu-item" href="/booking">
+          BOOKING
+        </a>
+        <a id="shop" className="menu-item" href="/shop">
+          SHOP
+        </a>
+        <a id="information" className="menu-item" href="/information">
+          INFORMATION
+        </a>
+      </Menu>
       <div>LOUDCROWDFEST INDEX</div>
     </div>
   );
