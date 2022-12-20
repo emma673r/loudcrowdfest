15c91929095c43ee746d3d4a91220a0120d2e3e2
emma673r <90177139+emma673r@users.noreply.github.com>
2022-12-21 00:04:51 +0100

    no id no class ?

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -32,34 +32,22 @@ function Layout() {
       <Menu right width={"100%"} noOverlay>
         {/* this is undefined but without it it doesnt work ?? (emma) */}
         {/* <Link onClick={() => this.closeMenu()} id="home" className="menu-item" to={"/"}> */}
-        <div id="home" className="menu-item" onClick={goToHome}>
-          HOME
-        </div>
+        <div onClick={goToHome}>HOME</div>
         {/* </Link> */}
         {/* <Link onClick={() => this.closeMenu()} id="schedule" className="menu-item" to={"../schedule"}> */}
-        <div id="schedule" className="menu-item" onClick={goToSche}>
-          SCHEDULE
-        </div>
+        <div onClick={goToSche}>SCHEDULE</div>
         {/* </Link> */}
         {/* <Link onClick={() => this.closeMenu()} id="lineup" className="menu-item" to={"../lineup"}> */}
-        <div id="lineup" className="menu-item" onClick={goToLine}>
-          LINE-UP
-        </div>
+        <div onClick={goToLine}>LINE-UP</div>
         {/* </Link> */}
         {/* <Link onClick={() => this.closeMenu()} id="booking" className="menu-item" to={"../booking"}> */}
-        <div id="booking" className="menu-item" onClick={goToBook}>
-          BOOKING
-        </div>
+        <div onClick={goToBook}>BOOKING</div>
         {/* </Link> */}
         {/* <Link onClick={() => this.closeMenu()} id="shop" className="menu-item" to={"../noPage"}> */}
-        <div id="shop" className="menu-item" onClick={goToNo}>
-          SHOP
-        </div>
+        <div onClick={goToNo}>SHOP</div>
         {/* </Link> */}
         {/* <Link onClick={() => this.closeMenu()} id="information" className="menu-item" to={"../information"}> */}
-        <div id="information" className="menu-item" onClick={goToInfo}>
-          INFORMATION
-        </div>
+        <div onClick={goToInfo}>INFORMATION</div>
         {/* </Link> */}
       </Menu>
       <Outlet />
