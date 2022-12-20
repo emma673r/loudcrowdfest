f6fca59d506c4861c9441ea0331bb4a09c13f204
emma673r <90177139+emma673r@users.noreply.github.com>
2022-12-20 23:55:33 +0100

    burger links please work on netlify

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 import { slide as Menu } from "react-burger-menu";
 import ReactDOM from "react-dom/client";
 import "../../src/burger-menu-style.css";
 import LogoWhite from "../components/img-components/LogoWhite";
 
 function Layout() {
+  const navigate = useNavigate();
+  function goToHome() {
+    navigate("/");
+  }
+  function goToSche() {
+    navigate("../schedule");
+  }
+  function goToLine() {
+    navigate("../lineup");
+  }
+  function goToBook() {
+    navigate("../booking");
+  }
+  function goToNo() {
+    navigate("../noPage");
+  }
+  function goToInfo() {
+    navigate("../information");
+  }
   return (
     <div className="menuContent">
       <LogoWhite></LogoWhite>
@@ -13,22 +32,22 @@ function Layout() {
       <Menu right width={"100%"} noOverlay>
         {/* this is undefined but without it it doesnt work ?? (emma) */}
         <Link onClick={() => this.closeMenu()} id="home" className="menu-item" to={"/"}>
-          HOME
+          <div onClick={goToHome}>HOME</div>
         </Link>
-        <Link onClick={() => this.closeMenu()} id="schedule" className="menu-item" to={"../schedule"}>
-          SCHEDULE
+        <Link onClick={() => this.closeMenu()} id="schedule" className="menu-item" to={""}>
+          <div onClick={goToSche}>SCHEDULE</div>
         </Link>
-        <Link onClick={() => this.closeMenu()} id="lineup" className="menu-item" to={"../lineup"}>
-          LINE-UP
+        <Link onClick={() => this.closeMenu()} id="lineup" className="menu-item" to={""}>
+          <div onClick={goToLine}>LINE-UP</div>
         </Link>
-        <Link onClick={() => this.closeMenu()} id="booking" className="menu-item" to={"../booking"}>
-          BOOKING
+        <Link onClick={() => this.closeMenu()} id="booking" className="menu-item" to={""}>
+          <div onClick={goToBook}>BOOKING</div>
         </Link>
-        <Link onClick={() => this.closeMenu()} id="shop" className="menu-item" to={"../noPage"}>
-          SHOP
+        <Link onClick={() => this.closeMenu()} id="shop" className="menu-item" to={""}>
+          <div onClick={goToNo}>SHOP</div>
         </Link>
-        <Link onClick={() => this.closeMenu()} id="information" className="menu-item" to={"../information"}>
-          INFORMATION
+        <Link onClick={() => this.closeMenu()} id="information" className="menu-item" to={""}>
+          <div onClick={goToInfo}>INFORMATION</div>
         </Link>
       </Menu>
       <Outlet />
